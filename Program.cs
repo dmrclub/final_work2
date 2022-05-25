3b577cc641c8c67d48ad4afe93600c2305cf1e5a
«dima <dmrclub18@gmail.com>
2022-05-25 18:36:45 +0300

    Создаем цикл который находит количество строк удолетворяющих условию задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// 
 string [] SpecifStrArray = {"lincol","dif","ar","mur","duncan","rom","in"};
+
+uint NumbersOfCells = 0;
+
+for (int i = 0; i < SpecifStrArray.Length; i++)
+{
+    if (SpecifStrArray[i].Length<=3)
+    {
+        NumbersOfCells ++;
+    }
+}
+
+
